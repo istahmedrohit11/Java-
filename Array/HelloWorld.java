d0d3c380056ec8028ed2b19dcebae6e908a9c7b3
Istiaque Ahmed Rohit <istiaquerohit713two@gmail.com>
2025-01-20 22:31:06 +0600

    Array normal code

--- Array/HelloWorld.java
+++ Array/HelloWorld.java
@@ -1,9 +1,18 @@
-import java.util.*;
+import java.util.Scanner;
 class HelloWorld {
     public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
-    String name = sc.nextLine();
-    System.out.println(name);       
-    
+        int [] marks= new int[5] ;
+        // declaration  //Memory allocation
+        //or
+        //int []marks={10,20,30,40,50};
+        marks[0]=100;
+        marks[1]=200;
+        marks[2]=300;
+        int i;
+        for(i=0;i<5;i++){
+            System.out.println(marks[i]);
+        }
+       }
     } 
-}
+
+
