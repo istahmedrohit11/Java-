c0c58c588c5116236a0907d4c1de735fdda8376b
Istiaque Ahmed Rohit <istiaquerohit713two@gmail.com>
2025-02-18 23:24:01 +0600

    One

--- Object Oriented Programming/Variables/HelloWorld.java
+++ Object Oriented Programming/Variables/HelloWorld.java
@@ -1,9 +1,20 @@
-import java.util.*;
-class HelloWorld {
-    public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
-    String name = sc.nextLine();
-    System.out.println(name);       
-    
-    } 
+class Student{
+    static String name="ULAB";// Static variable shared among all instances
+    int ID;// Instance variable
+    void display(){
+        int a=10;// Local variable
+        System.out.println("Varsity:"+name+"\nId:"+ID);
+    }
+}
+public class Hello{
+    public static void main(String[] args){
+        Student s1= new Student();
+        s1.ID=101;
+        s1.display();
+
+        Student.name="AIUB";// Change static variable name
+        //Since name is static, changing Student.name will affect all Student objects.
+        s1.display();
+
+    }
 }
