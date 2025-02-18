07540bad2412b727510b360d1faaa84fe7756317
Istiaque Ahmed Rohit <istiaquerohit713two@gmail.com>
2025-02-19 00:45:36 +0600

    example (1)

--- Object Oriented Programming/Use Same Memory/HelloWorld.java
+++ Object Oriented Programming/Use Same Memory/HelloWorld.java
@@ -1,9 +1,15 @@
-import java.util.*;
-class HelloWorld {
-    public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
-    String name = sc.nextLine();
-    System.out.println(name);       
-    
-    } 
+class Counter{
+    static int count=0;
+    Counter(){
+        count++;
+        System.out.print(Counter.count+"\t");
+    }
 }
+public class Hello{
+    public static void main(String[] args){
+        Counter c1=new Counter();
+        Counter c2=new Counter();
+        Counter c3=new Counter();
+    }
+}
+// output :1 2 3
